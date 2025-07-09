7e853b2d3ab8928297b06cc3304281232b52dcef
Jay <jay.zhangjunjie@outlook.com>
2025-07-10 01:20:07 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+Docker For ROS1
+
+自用的docker ROS1镜像，配置了一些常用的工具和命令，支持Remote-SSH
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###
+ # @Author       : Jay jay.zhangjunjie@outlook.com
+ # @Date         : 2025-07-08 23:32:06
+ # @LastEditTime : 2025-07-10 00:17:24
+ # @LastEditors  : Jay jay.zhangjunjie@outlook.com
+ # @Description  : 
+### 
+
+# cros 为镜像名称
+docker build --platform=linux/amd64 -t cros:latest . 
+
+# 后续如何创建实例
+# my_cros为实例名称 cros为镜像名称
+# docker run -it -e DISPLAY=${HOSTNAME}:0 -v /tmp/.X11-unix:/tmp/.X11-unix  --name my_cros cros zsh
+
+# 如何运行
+# docker exec -it my_cros
